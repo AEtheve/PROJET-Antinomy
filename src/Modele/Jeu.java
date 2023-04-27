cf01dfe07291c7b8893eb5c9341365b08d6dade5
Esteban Brnd <106627321+EstebanBrnd@users.noreply.github.com>
2023-04-27 13:47:55 +0200

    Créations du jeu de cartes

--- src/Modele/Jeu.java
+++ src/Modele/Jeu.java
@@ -1,8 +1,10 @@
 package Modele;
 
-
+import javax.xml.crypto.dsig.keyinfo.RetrievalMethod;
+import Modele.Carte;
 
 public class Jeu {
+    Carte[] cartes;
     
     Joueur p1, p2;
     Plateau plateau;
@@ -14,8 +16,29 @@ public class Jeu {
         this.plateau = plateau;
     }
 
-    void jouer(Joueur j, Coup c){
+    public Carte[] CréerCartes() {
+        int i=1;
+        int pos =0;
 
+        this.cartes = new Carte[16];
+        for (int valeur =1; valeur<=4; valeur++){
+            for (int symbole =1; symbole<=4; symbole++){
+                cartes[pos]=new Carte(symbole, i, valeur,0, false, false);
+                pos++;
+                i=(i+1)%4;
+            }
+        }
+        return cartes;
+    }
+
+    public void afficherCartes(){
+        for (int i=0; i<16; i++){
+            System.out.println("[C :"+cartes[i].getCouleur()+", S :"+cartes[i].getSymbole()+", V :"+cartes[i].getValeur()+" ]");
+        }
+    }
+
+    void jouer(Joueur j, Coup c){
+        plateau.jouer(c);
     }
 
 
