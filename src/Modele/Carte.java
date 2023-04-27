ff47c747d6c2aae2829be79c5ffc0c6262360b5b
Esteban Brnd <106627321+EstebanBrnd@users.noreply.github.com>
2023-04-27 10:41:43 +0200

    Update Carte.java

--- src/Modele/Carte.java
+++ src/Modele/Carte.java
@@ -7,9 +7,9 @@ public class Carte {
     public Carte(int symbole, int couleur_carte, int valeur_carte,int index,boolean retournee1,boolean retournee2){
         this.index = (byte)index;
         type = 0;
-        type += symbole << 6;
-        type += couleur_carte << 4;
-        type += valeur_carte << 2;
+        type += (symbole-1) << 6;
+        type += (couleur_carte-1) << 4;
+        type += (valeur_carte-1) << 2;
         if (retournee1) type += 0b1 << 1;
         else type += 0 << 1;
         if (retournee2) type += 0b1;
