09f94b62728b757223640497f4fa738b3521c51b
Esteban Brnd <106627321+EstebanBrnd@users.noreply.github.com>
2023-04-27 14:10:09 +0200

    Fin de création du jeu de cartes

--- src/Modele/Jeu.java
+++ src/Modele/Jeu.java
@@ -25,8 +25,11 @@ public class Jeu {
             for (int symbole =1; symbole<=4; symbole++){
                 cartes[pos]=new Carte(symbole, i, valeur,0, false, false);
                 pos++;
-                i=(i+1)%4;
+                i=(i+1)%5;
+                if(i==0) i++;
             }
+            i=(i+1)%5;
+            if(i==0) i++;
         }
         return cartes;
     }
@@ -35,6 +38,21 @@ public class Jeu {
         for (int i=0; i<16; i++){
             System.out.println("[C :"+cartes[i].getCouleur()+", S :"+cartes[i].getSymbole()+", V :"+cartes[i].getValeur()+" ]");
         }
+    }
+
+    public boolean verifierJeuDeCartes(){
+        for (int i=0;i<16;i++){
+            for (int j=0;j<16;j++){
+                if (i!=j){
+                    int verif=0;
+                    if (cartes[i].getCouleur()==cartes[j].getCouleur()) verif++;
+                    if (cartes[i].getSymbole()==cartes[j].getSymbole()) verif++;
+                    if (cartes[i].getValeur()==cartes[j].getValeur()) verif++;
+                    if (verif>1) return false;
+                }
+            }
+        }
+        return true;
     }
 
     void jouer(Joueur j, Coup c){
