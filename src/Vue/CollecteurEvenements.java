9e427ad15cf9d3cccf76218e75999a5fbd65c62a
Esteban Brnd <106627321+EstebanBrnd@users.noreply.github.com>
2023-04-29 23:48:02 +0200

    Erreur de push (fichiers inutiles)

--- src/Vue/CollecteurEvenements.java
+++
@@ -1,7 +0,0 @@
-package Vue;
-
-public interface CollecteurEvenements {
-	void onClick(int x, int y);
-	void onKeyPress(int keyCode);
-	//void ajouteInterfaceUtilisateur(InterfaceUtilisateur vue);
-}
