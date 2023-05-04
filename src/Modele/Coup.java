6fd29423f27871567b051a7046bb6ed6cd1284c8
Esteban Brnd <106627321+EstebanBrnd@users.noreply.github.com>
2023-05-04 11:15:39 +0200

    Ajout des fonctions de validation de coups

--- src/Modele/Coup.java
+++ src/Modele/Coup.java
@@ -5,14 +5,74 @@ public class Coup {
     private final static int SWAP_DROIT = 1;
     private final static int SWAP_GAUCHE = 2;
 
-    private int type;
-    Carte carte_main, carte_plateau;
-    Main m;
-    Carte swap [];
+    private byte type; 
+    private byte carte_main, carte_plateau;
 
-    public Coup (int type){
-        this.type = type;
+
+    public Coup (int type, int carte_main, int carte_plateau){
+        if (type==ECHANGE){
+            this.type = (byte) type;
+            this.carte_main = (byte) carte_main;
+            this.carte_plateau = (byte) carte_plateau;
+        }
     }
+
+    public Coup(int type, int joueur){
+        if (type==SWAP_DROIT || type==SWAP_GAUCHE){
+            this.type = (byte) type;
+            this.carte_main = (byte) joueur;
+        }
+    }
+
+    public byte getType(){
+        return this.type;
+    }
+
+    private Boolean estSwapValide(Jeu j){
+        int pos_sc = j.getDeck().getSceptre(j.getTour());
+        
+        switch(type){
+            case SWAP_DROIT:
+                if (pos_sc>=12) return false;
+                return true;
+            case SWAP_GAUCHE:
+                if (pos_sc<=3) return false;
+                return true;
+            default:
+                throw new IllegalArgumentException("Position du sceptre invalide");
+        }
+    }
+
+    private Boolean estEchangeValide(Jeu j){
+        Carte[] plateau = j.getDeck().getPlateau();
+        Carte[] main =  j.getMain(j.getTour());
+
+        for (int i = 0; i < plateau.length; i++){
+            if (plateau[i].getIndex()==this.carte_plateau){
+                for (int k = 0; k < main.length; k++){
+                    if (main[k].getIndex()==this.carte_main){
+                        if (main[k].getColor()==plateau[i].getColor() || main[k].getSymbol()==plateau[i].getSymbol() || j.getDeck().getSceptre(j.getTour())+main[k].getValue()==i){
+                            return true;
+                        }
+                        return false;
+                    }
+                }
+            }
+        }
+        throw new IllegalArgumentException("Carte non trouvée");
+    }
+
+    public Boolean estCoupValide(Jeu j){
+        if (this.type==ECHANGE){
+            return estEchangeValide(j);
+        }
+        else if (this.type==SWAP_DROIT || this.type==SWAP_GAUCHE){
+            return estSwapValide(j);
+        }
+        throw new IllegalArgumentException("Type de coup invalide");
+    }
+
+
 
     
 }
