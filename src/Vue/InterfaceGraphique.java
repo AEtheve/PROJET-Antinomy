11263deb12fe86276cc19f700b563125dc4c5f40
AEtheve <30652308+AEtheve@users.noreply.github.com>
2023-05-04 22:48:05 +0200

    Update InterfaceGraphique.java

--- src/Vue/InterfaceGraphique.java
+++ src/Vue/InterfaceGraphique.java
@@ -1,53 +1,81 @@
 package Vue;
 
+import Modele.Carte;
+import Modele.Compteur;
+import Modele.Coup;
+import Modele.Jeu;
+import Modele.Main;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseListener;
+import java.io.File;
+import java.awt.event.MouseEvent;
 
-import Modele.Jeu;
+import java.util.Scanner;
+
 import Controleur.ControleurJoueur;
+import Global.Configuration;
 
-public class InterfaceGraphique implements Runnable{
+public class InterfaceGraphique implements Runnable, InterfaceUtilisateur{
     Jeu jeu;
-    JFrame fenetre;
-    boolean maximized;
     ControleurJoueur ctrl;
+    boolean maximized;
+    JFrame fenetre;
+    
 
-    InterfaceGraphique(Jeu j,ControleurJoueur c){
-        this.jeu = j;
-        this.ctrl = c;
+    public InterfaceGraphique(Jeu jeu, ControleurJoueur ctrl){
+        this.jeu = jeu;
+        this.ctrl = ctrl;
     }
 
-    public static void demarrer(Jeu j,ControleurJoueur c){
-        InterfaceGraphique ig = new InterfaceGraphique(j,c);
-        ctrl.ajoute(ig);    
-        SwingUtilities.invokeLater(ig);
-    }
+    public static void demarrer(Jeu j, ControleurJoueur ctrl){
+		InterfaceGraphique vue = new InterfaceGraphique(j, ctrl);
+        ctrl.ajouteInterfaceUtilisateur(vue);
+		SwingUtilities.invokeLater(vue);
+	}
 
-    public void toogleFullScreen(){
-        GraphicsEnvironment env = GraphicsEnvironment.getLocalGraphicsEnvironment();
-        GraphicsDevice device = env.getDefaultScreenDevice();
-        if(maximized){
-            device.setFullScreenWindow(null);
-            maximized = false;
-        } else {
-            device.setFullScreenWindow(fenetre);
-            maximized = true;
-        }
-    }
+    public void toggleFullscreen() {
+		GraphicsEnvironment env = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		GraphicsDevice device = env.getDefaultScreenDevice();
+		if (maximized) {
+			device.setFullScreenWindow(null);
+			maximized = false;
+		} else {
+			device.setFullScreenWindow(fenetre);
+			maximized = true;
+		}
+	}
 
     public void run(){
         creationFenetre();
     }
 
-    void creationFenetre(){
-        fenetre = new JFrame("Antinomy");
-
-        // fenetre.add(new DeckGraphique());
-
-        fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    private void creationFenetre(){
+        // Création de la fenêtre principale
+        Configuration.info("Creation de la fenetre principale");
+        fenetre = new JFrame();
+        fenetre.setTitle("Antinomy");
         fenetre.setSize(800, 600);
+        fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         fenetre.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        fenetre.setResizable(true);
+        fenetre.getContentPane().setBackground(new Color(199, 175, 161));
         fenetre.setVisible(true);
+        Configuration.info("Fenetre principale créée");
     }
 
+
+    @Override
+    public void miseAJour(){
+    }
+
+    @Override
+    public void animeCoup(Coup coup) {
+        throw new UnsupportedOperationException("Unimplemented method 'animeCoup'");
+    }
 }
