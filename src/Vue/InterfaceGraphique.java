0b5000f6c39415c49d107a9cd825e07456ad90f3
AEtheve <30652308+AEtheve@users.noreply.github.com>
2023-05-10 20:08:51 +0200

    Revert "Maj graphique"

--- src/Vue/InterfaceGraphique.java
+++ src/Vue/InterfaceGraphique.java
@@ -130,8 +130,6 @@ public class InterfaceGraphique implements Runnable, InterfaceUtilisateur {
         creationFenetre();
 
         continuumGraphique = new ContinuumGraphique(jeu, ctrl, imagesCache);
-        continuumGraphique.initializeComponents();
-        
         mainMenu mainMenu = new mainMenu(fenetre, continuumGraphique);
 
         fenetre.setContentPane(mainMenu);
