a26df44247a3ed74fb6c67713de49a41d8542b96
AEtheve <30652308+AEtheve@users.noreply.github.com>
2023-05-20 15:46:47 +0200

    MAJ IHM: Daltonisme

--- src/Global/Configuration.java
+++ src/Global/Configuration.java
@@ -10,7 +10,10 @@ import Structures.Sequence;
 import Structures.SequenceListe;
 import Structures.SequenceTableau;
 
+import java.awt.Color;
 import java.awt.Image;
+import java.awt.image.BufferedImage;
+
 
 public class Configuration {
 	static Configuration instance = null;
@@ -18,7 +21,7 @@ public class Configuration {
 	public final static String typeInterface = "Graphique";
 	public static String theme = "Images";
 	String typeSequences;
-	boolean fixedSeed = true;
+	boolean fixedSeed = false;
 	public final static int MAX = 5;
 
 	public static int difficulteIA = 1; // 1 : Aléatoire
@@ -55,11 +58,14 @@ public class Configuration {
 		System.exit(1);
 	}
 
+
 	public static Image lisImage(String nom) {
 		InputStream in = Configuration.ouvre(theme + "/" + nom + ".png");
         Configuration.info("Chargement de l'image " + nom);
         try {
-            return ImageIO.read(in);
+			BufferedImage image = ImageIO.read(in);
+			// image = Daltonisme.apply(image, Daltonisme.Type.TRITANOMALY);
+			return image;
         } catch (Exception e) {
 			Configuration.alerte("Impossible de charger l'image " + nom);
         }
@@ -116,6 +122,104 @@ public class Configuration {
 		return instance;
 	}
 	
+}
 
 
+class Daltonisme {
+    public enum Type {
+        PROTANOPIA,
+        DEUTERANOPIA,
+        TRITANOPIA,
+        // Autres types de daltonisme
+        
+        // Complète la liste des types de daltonisme ici
+        DEUTERANOMALY,
+        PROTANOMALY,
+        TRITANOMALY
+    }
+
+    public static BufferedImage apply(BufferedImage image, Type type) {
+        int width = image.getWidth();
+        int height = image.getHeight();
+        BufferedImage newImg = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                Color color = new Color(image.getRGB(x, y), true);
+                int red = color.getRed();
+                int green = color.getGreen();
+                int blue = color.getBlue();
+                int alpha = color.getAlpha();
+
+				Color newColor;
+                switch (type) {
+                    case PROTANOPIA:
+                        newColor = convertProtanopia(red, green, blue, alpha);
+                        break;
+                    case DEUTERANOPIA:
+                        newColor = convertDeuteranopia(red, green, blue, alpha);
+                        break;
+                    case TRITANOPIA:
+                        newColor = convertTritanopia(red, green, blue, alpha);
+                        break;
+                    case DEUTERANOMALY:
+                        newColor = convertDeuteranomaly(red, green, blue, alpha);
+                        break;
+                    case PROTANOMALY:
+                        newColor = convertProtanomaly(red, green, blue ,alpha);
+                        break;
+                    case TRITANOMALY:
+                        newColor = convertTritanomaly(red, green, blue, alpha);
+                        break;
+                    default:
+                        newColor = new Color(red, green, blue, alpha);
+                        break;
+                }
+                newImg.setRGB(x, y, newColor.getRGB());
+            }
+        }
+        return newImg;
+    }
+
+	private static Color convertProtanopia(int red, int green, int blue, int alpha) {
+		int newRed = (int) (0.56667 * green + 0.43333 * blue);
+		int newGreen = (int) (0.55833 * red + 0.44167 * blue);
+		int newBlue = (int) (0.24167 * red + 0.75833 * green);
+		return new Color(newRed, newGreen, newBlue, alpha);
+	}
+
+	private static Color convertDeuteranopia(int red, int green, int blue, int alpha) {
+		int newRed = (int) (0.625 * green + 0.375 * blue);
+		int newGreen = (int) (0.7 * red + 0.3 * blue);
+		int newBlue = (int) (0.3 * red + 0.7 * green);
+		return new Color(newRed, newGreen, newBlue, alpha);
+	}
+
+	private static Color convertTritanopia(int red, int green, int blue, int alpha) {
+		int newRed = (int) (0.95 * green + 0.05 * blue);
+		int newGreen = (int) (0.43333 * red + 0.56667 * blue);
+		int newBlue = (int) (0.475 * red + 0.525 * green);
+		return new Color(newRed, newGreen, newBlue, alpha);
+	}
+
+	private static Color convertDeuteranomaly(int red, int green, int blue, int alpha) {
+		int newRed = (int) (0.8 * green + 0.2 * blue);
+		int newGreen = (int) (0.25833 * red + 0.74167 * blue);
+		int newBlue = (int) (0.14167 * red + 0.85833 * green);
+		return new Color(newRed, newGreen, newBlue, alpha);
+	}
+
+	private static Color convertProtanomaly(int red, int green, int blue, int alpha) {
+		int newRed = (int) (0.81667 * green + 0.18333 * blue);
+		int newGreen = (int) (0.33333 * red + 0.66667 * blue);
+		int newBlue = (int) (0.00833 * red + 0.99167 * green);
+		return new Color(newRed, newGreen, newBlue, alpha);
+	}
+
+	private static Color convertTritanomaly(int red, int green, int blue, int alpha) {
+		int newRed = (int) (0.96667 * green + 0.03333 * blue);
+		int newGreen = (int) (0.05 * red + 0.95 * blue);
+		int newBlue = (int) (0 * red + 0.73333 * green);
+		return new Color(newRed, newGreen, newBlue, alpha);
+	}
 }
